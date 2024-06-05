62dda73d380fa62adbe4190b8004a9067f310aed
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-05 23:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function sortArticles(arr)
+{
+    return arr.sort((a, b) => {
+        const stripArticle = str => str.replace(/^(a |an |the )/i, '').trim();
+        const titleA = stripArticle(a.toLowerCase());
+        const titleB = stripArticle(b.toLowerCase());
+        return titleA.localeCompare(titleB);
+    });
+}
+
+const sortedArticles = sortArticles(bands);
+
+const ul = document.getElementById("band");
+
+sortedArticles.forEach(article => {
+    const li = document.createElement('li');
+            li.textContent = article;
+            ul.appendChild(li);
+});
