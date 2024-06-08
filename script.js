271b902f317ca8b36a4b4cfd219a1c31c3e59813
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-08 14:36:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function sortArticles(articles) {
     });
 }
 //const articles =  ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-const sortedArticles = sortArticles(bands);
+const sortedArticles = sortArticles(band);
 let ul = document.getElementById("band");
 
 for(let i=0; i<sortedArticles.length; i++)
