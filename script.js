7a8375995e0208c4323d06e9e3bd1b39b4afe32d
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-08 13:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-//your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-function sortingArticles(articles) {
-            return articles.sort((a, b) =>{
-				let titleA = stripArticle(a.toLowerCase());
-				let titleB = sortArticles(b.toLowerCase());
 
-				if(titleA < titleB) return -1;
-				if(titleA > titleB) return 1;
-				return 0;
-			});
-        }
+function stripArticle(title) {
+    return title.replace(/^(a |an |the )/i, '').trim();
+}
 
-function stripArticle(title){
-	return title.replace(/^(a | an | the)/i, " ").trim();
-}	
+function sortArticles(articles) {
+    return articles.sort((a, b) => {
+        let titleA = stripArticle(a.toLowerCase());
+        let titleB = stripArticle(b.toLowerCase());
+        if (titleA < titleB) return -1;
+        if (titleA > titleB) return 1;
+        return 0;
+    });
+}
+const articles =  ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const sortedArticles = sortArticles(articles);
+let ul = document.getElementById("band");
 
+for(let i=0; i<sortedArticles.length; i++)
+{
+    let li = document.createElement("li");
+    li.innerText = sortedArticles[i];
+    ul.append(li);
+}
 
-let arr = sortingArticles(bands);
-let ol = document.getElementById("band");
-for(let i=0; i<arr.length; i++)
-	{
-		li = document.createElement("li");
-		li.innerText = sortedArticles[i];
-		ol.append(li);
-	}
 
