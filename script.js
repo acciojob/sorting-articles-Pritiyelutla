1cf7e86450ecc93d6454337ade46b43724b36dbf
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-13 20:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-
 function stripArticle(title) {
-    return title.replace(/^(a |an |the )/i, '').trim();
+  return title.replace(/^(a |an |the )/i, '').trim();
 }
 
 function sortArticles(articles) {
-    return articles.sort((a, b) => {
-        let titleA = stripArticle(a.toLowerCase());
-        let titleB = stripArticle(b.toLowerCase());
-        if (titleA < titleB) return -1;
-        if (titleA > titleB) return 1;
-        return 0;
-    });
+  return articles.sort((a, b) => {
+      let titleA = stripArticle(a.toLowerCase());
+      let titleB = stripArticle(b.toLowerCase());
+      if (titleA < titleB) return -1;
+      if (titleA > titleB) return 1;
+      return 0;
+  });
 }
-//const articles =  ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-const sortedArticles = sortArticles(band);
+const articles =  ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+//console.log(articles);
+
+const sortedArticles = sortArticles(articles);
 let ul = document.getElementById("band");
 
 for(let i=0; i<sortedArticles.length; i++)
 {
-    let li = document.createElement("li");
-    li.innerText = sortedArticles[i];
-    ul.append(li);
+  let li = document.createElement("li");
+  li.innerText = sortedArticles[i];
+  ul.append(li);
 }
 
 
